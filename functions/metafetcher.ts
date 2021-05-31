9a6f73dee3faa9362ccbb5467d28a64917437e12
Shoaib Sharif <shoaib19j@gmail.com>
2021-05-31 10:42:57 -0700

    init

---
+++ functions/metafetcher.ts
@@ -0,0 +1,56 @@
+import { Handler } from "@netlify/functions";
+import fetch from "isomorphic-unfetch";
+import * as cheerio from "cheerio";
+
+export const handler: Handler = async (event, context) => {
+  //   console.log("queryStringParameters", event.queryStringParameters);
+
+  if (event.httpMethod === "GET") {
+    try {
+      let result;
+      if (event.queryStringParameters?.url) {
+        const res = await fetch(event.queryStringParameters?.url);
+        const content = await res.text();
+        const $ = cheerio.load(content);
+        const head = $("head");
+        const title = head.find("title").text();
+        const description = head
+          .find('meta[name="description"]')
+          .attr("content");
+        const image = head.find('meta[property="og:image"]').attr("content");
+        result = { title, description, image: { url: image } };
+      } else {
+        return {
+          statusCode: 404,
+          body: JSON.stringify({
+            message: "Please enter an url with url query parameter",
+          }),
+        };
+      }
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          success: 1,
+          result,
+        }),
+      };
+    } catch (error) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          success: 0,
+          message: "Only absolute URLs are supported via query parameters url",
+        }),
+      };
+    }
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        success: 0,
+        message: "Only GET method is allowed",
+      }),
+    };
+  }
+};
